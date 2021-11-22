d77d0ab822419ad65e0441ee1f0594398ac478ae
wangzhenguo <wangzhenguo@360.cn>
2021-11-22 18:26:36 +0800

    Site updated: 2021-11-22 18:26:35

---
+++ js/IndexToTop.js
@@ -0,0 +1,1 @@
+addBackToTop({diameter:56,backgroundColor:"#53b3fa",textColor:"#fff",scrollDuration:300});
---
+++ js/jquery.toTop.js
@@ -0,0 +1,1 @@
+$(function(){var o=500;var t=document.body.clientHeight;$(".toc-bottom").click(function(){$("html,body").animate({scrollTop:"0px"},o)})});
